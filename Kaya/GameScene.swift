23516149a800a7a23c69da5528f4c10977039c6b
Robert Ahlberg <rob.ahlberg@gmail.com>
2022-12-01 22:56:09 +0100

    Working for now

--- Kaya/GameScene.swift
+++ Kaya/GameScene.swift
@@ -2,87 +2,167 @@
 //  GameScene.swift
 //  Kaya
 //
-//  Created by Robert Ahlberg on 2022-11-20.
+//  Created by Robert Ahlberg on 2022-11-24.
 //
 
 import SpriteKit
-import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    // MARK: - Variables
+    private var gameStarted = false
+    private var gameOver = false
+    private let obstacleWidth = 60
+    private let gap = 140
+    private let obstacleMoveDuration = 3.5
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    private var canScore = false
+    private var score = 0 {
+        didSet {
+            scoreLabel.text = String(score)
+        }
+    }
+    
+    private lazy var scoreLabel: SKLabelNode = {
+        let label = SKLabelNode()
+        label.fontSize = 54
+        label.text = "0"
+        label.position = CGPoint(x: self.size.width / 2, y: self.size.height - 150)
+        return label
+    }()
+    
+    private lazy var statusLabel: SKLabelNode = {
+        let label = SKLabelNode()
+        label.fontSize = 48
+        label.text = "TAP TO BEGIN"
+        label.fontName = "AvenirNext-Bold"
+        label.fontColor = UIColor.yellow
+        label.position = CGPoint(x: self.size.width / 2, y: self.size.height / 2 + 40)
+        return label
+    }()
+    
+    private lazy var rocket: SKSpriteNode = {
+        let rocket = SKSpriteNode(imageNamed: "rocket")
+        print("rocket.size \(rocket.size)")
+        rocket.physicsBody = SKPhysicsBody(rectangleOf: rocket.size)
+        rocket.position = CGPoint(
+            x: self.size.width / 2 - rocket.frame.size.width / 2,
+            y: self.size.height / 2 - rocket.frame.size.height / 2)
+        return rocket
+    }()
+    
+    private lazy var upperObstacle: SKShapeNode = {
+        let size = CGSize(width: obstacleWidth, height: Int(self.size.height) / 2 - (gap / 2))
+        let obstacle = SKShapeNode(rectOf: size)
+        obstacle.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: obstacleWidth, height: Int(self.size.height) / 2 - (gap / 2)))
+        obstacle.physicsBody?.isDynamic = false
+        obstacle.fillColor = .blue
+        obstacle.lineWidth = 0
+        
+        obstacle.position.x = self.size.width + CGFloat(obstacleWidth)
+        obstacle.position.y = self.size.height - obstacle.frame.size.height / 2
+        
+        return obstacle
+    }()
+    
+    private lazy var lowerObstacle: SKShapeNode = {
+        let size = CGSize(width: obstacleWidth, height: Int(self.size.height) / 2 - (gap / 2))
+        let obstacle = SKShapeNode(rectOf: size)
+        obstacle.physicsBody = SKPhysicsBody(rectangleOf: size)
+        obstacle.physicsBody?.isDynamic = false
+        obstacle.fillColor = .blue
+        obstacle.lineWidth = 0
+        
+        obstacle.position.x = self.size.width + CGFloat(obstacleWidth)
+        obstacle.position.y = 0 + obstacle.frame.size.height / 2
+        
+        return obstacle
+    }()
+    
+    // MARK: - Lifecycle
+    override func sceneDidLoad() {
+        physicsWorld.gravity = CGVector(dx: 0, dy: -7)
+        physicsWorld.contactDelegate = self
+        
+        let worldEdge = SKPhysicsBody(edgeLoopFrom: self.frame)
+        worldEdge.friction = 0
+        worldEdge.restitution = 0
+        physicsBody = worldEdge
+        
+        addChild(scoreLabel)
+        addChild(statusLabel)
+        
+        addChild(rocket)
+        
+        addChild(upperObstacle)
+        addChild(lowerObstacle)
+        
+        // Setup physics contact
+        rocket.physicsBody?.contactTestBitMask = rocket.physicsBody!.collisionBitMask
+        
+        let openScoreAction = SKAction.run {
+            self.canScore = true
+        }
+        let moveAction = SKAction.moveTo(x: CGFloat(-1 - self.obstacleWidth / 2), duration: obstacleMoveDuration)
+        let resetPositionAction = SKAction.run {
+            self.upperObstacle.position.x = self.size.width + CGFloat(self.obstacleWidth)
+            self.upperObstacle.position.y = self.size.height - self.upperObstacle.frame.size.height / 2
+            
+            self.lowerObstacle.position.x = self.size.width + CGFloat(self.obstacleWidth)
+            self.lowerObstacle.position.y = 0 + self.lowerObstacle.frame.size.height / 2
+        }
+        
+        let sequence = SKAction.sequence([
+            openScoreAction,
+            moveAction,
+            resetPositionAction
+        ])
+        
+        let repeatAction = SKAction.repeatForever(sequence)
+        
+        upperObstacle.run(repeatAction)
+        lowerObstacle.run(repeatAction)
+    }
     
     override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        view.isPaused = true
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
+    // MARK: - Game loop
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        if (upperObstacle.position.x + upperObstacle.frame.size.width / 2 < rocket.position.x - rocket.frame.size.width / 2) {
+            if canScore {
+                score += 1
+            }
+            canScore = false
+        }
+        
+        // Rotate ship according the velocity
+        // Reset rotation
+        rocket.physicsBody?.angularVelocity = 0
+        rocket.zRotation = 0
+        let angle = atan2(rocket.physicsBody?.velocity.dy ?? 0, size.width / obstacleMoveDuration)
+        let rotateAction = SKAction.rotate(byAngle: angle, duration: 0.7)
+        rocket.run(rotateAction)
+    }
+    
+    // MARK: - Input
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if !gameStarted && !gameOver {
+            view?.isPaused = false
+            statusLabel.isHidden = true
+        }
+        
+        rocket.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+        rocket.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 28))
+        
+        super.touchesBegan(touches, with: event)
+    }
+    
+    // MARK: - SKPhysicsContactDelegate
+    func didBegin(_ contact: SKPhysicsContact) {
+        statusLabel.text = "GAME OVER"
+        statusLabel.isHidden = false
+        gameOver = true
+        view?.isPaused = true
     }
 }
--- Kaya/GameViewController.swift
+++ Kaya/GameViewController.swift
@@ -10,27 +10,21 @@ import SpriteKit
 import GameplayKit
 
 class GameViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         
         if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
+            view.presentScene(GameScene())
             
             view.ignoresSiblingOrder = true
             
             view.showsFPS = true
             view.showsNodeCount = true
+            
+            view.presentScene(GameScene(size: view.bounds.size))
         }
     }
-
+    
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         if UIDevice.current.userInterfaceIdiom == .phone {
             return .allButUpsideDown
@@ -38,7 +32,7 @@ class GameViewController: UIViewController {
             return .all
         }
     }
-
+    
     override var prefersStatusBarHidden: Bool {
         return true
     }
